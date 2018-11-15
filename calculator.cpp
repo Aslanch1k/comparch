8895ebb1cdc927fde6a96786598e58db8bf080c4
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    add a multiplication operation
    In fact the `Calculator` class doesn't have member-variable, so we can
    mark `Add` and `Mul` as `static` and use them without an instance.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,11 @@
 #include "calculator.h"
 
+//IO-01
+//Dmytro Dudchyk
+//3 course
+//Linux is better then Windows
+//Ukreine is the gretest country
+
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
